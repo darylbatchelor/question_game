85ebb366ab50d4d71353b415f8700b9ed1de62c9
webdiptafenq <webdiptafenq@batchelors.id.au>
2016-04-21 20:39:07 +1000

    added favicon

--- pages/connect.php
+++ pages/connect.php
@@ -2,9 +2,9 @@
 
 
 $host = "localhost";
-$db_user = "root";
-$db_password = "";
-$db_name = "question_game";
+$db_user = "batchebr_guest";
+$db_password = "Ax3lsa11y!guest";
+$db_name = "batchebr_question_game";
 
 
 $con = mysqli_connect($host, $db_user, $db_password, $db_name) or die("Could not connect to the database");
